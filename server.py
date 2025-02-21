c66d6c7de870be0e6f74ac72fcac1068b090a5c2
Jon Radchenko <jonrad@gmail.com>
2025-02-21 16:16:17 -0500

    switched to pyproject to use uvx

--- server.py
+++ server.py
@@ -3,35 +3,36 @@ import libtmux
 import logging
 import os
 
-# Create an MCP server
-mcp = FastMCP("tmux", settings={"log_level": "DEBUG"})
+def main():
+    # Create an MCP server
+    mcp = FastMCP("tmux", settings={"log_level": "DEBUG"})
 
-# FastMCP sets its own logging, which we may want to override
-log_file = os.getenv("LOG_FILE", "default.log")
-if log_file:
-    file_handler = logging.FileHandler(log_file)
-    logging.root.handlers.append(file_handler)
-logger = logging.getLogger(__name__)
+    # FastMCP sets its own logging, which we may want to override
+    log_file = os.getenv("LOG_FILE", "default.log")
+    if log_file:
+        file_handler = logging.FileHandler(log_file)
+        logging.root.handlers.append(file_handler)
+    logger = logging.getLogger(__name__)
 
-# Connect to the tmux server
-server = libtmux.Server()
+    # Connect to the tmux server
+    server = libtmux.Server()
 
-logger.info("Starting tmux MCP server")
+    logger.info("Starting tmux MCP server")
 
-@mcp.tool(description="Run a generic command in tmux")
-def run_tmux_command(command: str, args: list[str]) -> str:
-    if command == 'tmux':
-        command = args.pop(0)
+    @mcp.tool(description="Run a generic command in tmux")
+    def run_tmux_command(command: str, args: list[str]) -> str:
+        if command == 'tmux':
+            command = args.pop(0)
 
-    logger.info(f"Run command: {command} {args}")
-    result = server.cmd(command, *args)
-    formatted = "\n".join(result.stdout)
-    logger.info(f"Result: {formatted}")
-    return result.stdout
+        logger.info(f"Run command: {command} {args}")
+        result = server.cmd(command, *args)
+        formatted = "\n".join(result.stdout)
+        logger.info(f"Result: {formatted}")
+        return result.stdout
 
-#@mcp.resource("tmux://pane/current", description="Gets the content of the current pane")
-@mcp.tool(description="Gets the content of the current pane")
-def get_current_pane_contents() -> str:
-    return server.panes[0].capture_pane(end="-10")
+    #@mcp.resource("tmux://pane/current", description="Gets the content of the current pane")
+    @mcp.tool(description="Gets the content of the current pane")
+    def get_current_pane_contents() -> str:
+        return server.panes[0].capture_pane(end="-10")
 
-mcp.run()
+    mcp.run()
